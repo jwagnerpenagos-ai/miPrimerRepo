73c27ee650909d6394aae50047718744b781f94f
Gato <jwagnerpenagos@gmail.com>
2026-06-09 19:39:30 -0500

    Modify Stripe API key and add connection string
    Updated the Stripe API key and added a connection string.

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,3 +1,5 @@
-﻿Console.WriteLine("Hello, Platzi!");
+Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_51M9n2uKZs8mLh7l3a5jl3a5jLh7l3a5j";
+string stripe_api_key = "sk_test_51M9n2uKZs8mLh7l3a5jl3a5jLh7l3a5jLh7l3a5";
+
+string testingConectionString = "Server=localhost;Database=miPrimerRepo;User Id=sa;Password=your_password;";
